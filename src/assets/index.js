6db143e7ab6699e73d95ffb7d2aa563626a3ce02
Ashita28 <125440800+Ashita28@users.noreply.github.com>
2025-02-12 16:44:17 +0530

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -27,7 +27,7 @@ import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
-import notes from "./notes.jpg";
+import notes from "./notes_logo.png";
 import pocketNotes from "./pocketnotes.png";
 
 
